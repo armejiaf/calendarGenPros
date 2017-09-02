d04d0e07edd0c31077b16c827dc4702b11c9dd0c
armejiaf <mejia561@hotmail.com>
2017-09-02 15:57:33 -0600

    create-react-app cleanse

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Form extends React.Component {
+	render(){
+		return(
+			<div>
+				Start
+			</div>
+		);
+	}
+}
+
+ReactDOM.render(
+	<Form/>,
+	document.getElementById('root')
+);
